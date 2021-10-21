564eee770260405844e42cf19c6dda37edcf1633
Timo <tklaasee@gmail.com>
2021-10-22 01:03:55 +0200

    Refactored component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import MomentUtils from '@date-io/moment';
 import {SnackbarProvider} from 'notistack';
 import {jssPreset, StylesProvider, ThemeProvider} from '@material-ui/core';
 import {MuiPickersUtilsProvider} from '@material-ui/pickers';
-import GlobalStyles from './components/GlobalStyles';
+import GlobalStyles from './components/root/GlobalStyles';
 import useSettings from './hooks/useSettings';
 import {createTheme} from './theme';
 import routes, {renderRoutes} from './routes';
 import {BrowserRouter} from 'react-router-dom';
-import CookiesNotification from "./components/CookiesNotification";
+import CookiesNotification from "./components/root/CookiesNotification";
 
 const jss = create({plugins: [...jssPreset().plugins, rtl()]});
 const history = createBrowserHistory();
