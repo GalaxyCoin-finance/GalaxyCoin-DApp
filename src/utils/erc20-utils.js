36f97e01afc62659acfd3e89d4052fff0deecfc8
Timo <tklaasee@gmail.com>
2021-10-22 21:00:19 +0200

    Added utils

---
+++ src/utils/erc20-utils.js
@@ -0,0 +1,5 @@
+
+
+export const getERC20Balance = () => {
+
+}
---
+++ src/utils/farmConfigs.js
@@ -0,0 +1,55 @@
+const farmConfigs = {
+    farms: [
+        {
+            "name": "mGLXY",
+            "composition": "mGLXY = GLXY (50%) / WMATIC + USDT + USDC + LINK + CRV (50%)",
+            "apy": "3000",
+            "address": "",
+            "tags": "GALAXY,MATIC,USDT,USDC,LINK,CRV"
+        },
+        {
+            "name": "GALAXY",
+            "composition": "GALAXY (100%)",
+            "apy": "500",
+            "address": "",
+            "tags": "GALAXY-X,SINGLE-ASSET"
+        },
+        {
+            "name": "GAX-MATIC",
+            "composition": "GAX-MATIC = GAX (50%) / WMATIC (50%)",
+            "apy": "4000",
+            "address": "",
+            "tags": "GALAXY-X,MATIC"
+        },
+        {
+            "name": "USDT",
+            "composition": "USDT (100%)",
+            "apy": "1000",
+            "address": "",
+            "tags": "USDT,STABLE,SINGLE-ASSET"
+        },
+        {
+            "name": "GAX",
+            "composition": "GAX (100%)",
+            "apy": "300",
+            "address": "",
+            "tags": "GALAXY-X,SINGLE-ASSET"
+        },
+        {
+            "name": "USDC",
+            "composition": "USDC (100%)",
+            "apy": "1000",
+            "address": "",
+            "tags": "USDC,STABLE,SINGLE-ASSET"
+        },
+        {
+            "name": "MATIC",
+            "composition": "MATIC (100%)",
+            "apy": "1000",
+            "address": "",
+            "tags": "MATIC,SINGLE-ASSET"
+        },
+    ]
+}
+
+module.exports = farmConfigs;
