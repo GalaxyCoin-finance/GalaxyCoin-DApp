6eed6700429dc7b50a21f89ad1c499abc9188147
Timo <tklaasee@gmail.com>
2021-10-26 16:15:28 +0200

    Added info

--- src/utils/farmConfigs.js
+++ src/utils/farmConfigs.js
@@ -1,55 +1,46 @@
-const farmConfigs = {
-    farms: [
-        {
-            "name": "mGLXY",
-            "composition": "mGLXY = GLXY (50%) / WMATIC + USDT + USDC + LINK + CRV (50%)",
-            "apy": "3000",
-            "address": "1",
-            "tags": "GALAXY,MATIC,USDT,USDC,LINK,CRV"
-        },
-        {
-            "name": "GALAXY",
-            "composition": "GALAXY (100%)",
-            "apy": "500",
-            "address": "2",
-            "tags": "GALAXY-X,SINGLE-ASSET"
-        },
-        {
-            "name": "GAX-MATIC",
-            "composition": "GAX-MATIC = GAX (50%) / WMATIC (50%)",
-            "apy": "4000",
-            "address": "3",
-            "tags": "GALAXY-X,MATIC"
-        },
-        {
-            "name": "USDT",
-            "composition": "USDT (100%)",
-            "apy": "1000",
-            "address": "4",
-            "tags": "USDT,STABLE,SINGLE-ASSET"
-        },
-        {
-            "name": "GAX",
-            "composition": "GAX (100%)",
-            "apy": "300",
-            "address": "5",
-            "tags": "GALAXY-X,SINGLE-ASSET"
-        },
-        {
-            "name": "USDC",
-            "composition": "USDC (100%)",
-            "apy": "1000",
-            "address": "6",
-            "tags": "USDC,STABLE,SINGLE-ASSET"
-        },
-        {
-            "name": "MATIC",
-            "composition": "MATIC (100%)",
-            "apy": "1000",
-            "address": "7",
-            "tags": "MATIC,SINGLE-ASSET"
-        },
-    ]
-}
+const farmConfigs = [
+    {
+        "name": "mGLXY",
+        "composition": "mGLXY = GLXY (50%) / WMATIC + USDT + USDC + LINK + CRV (50%)",
+        "pid": 0,
+        "tags": "GALAXY,MATIC,USDT,USDC,LINK,CRV"
+    },
+    {
+        "name": "GALAXY",
+        "composition": "GALAXY (100%)",
+        "address": "2",
+        "tags": "GALAXY-X,SINGLE-ASSET"
+    },
+    {
+        "name": "GAX-MATIC",
+        "composition": "GAX-MATIC = GAX (50%) / WMATIC (50%)",
+        "address": "3",
+        "tags": "GALAXY-X,MATIC"
+    },
+    {
+        "name": "USDT",
+        "composition": "USDT (100%)",
+        "address": "4",
+        "tags": "USDT,STABLE,SINGLE-ASSET"
+    },
+    {
+        "name": "GAX",
+        "composition": "GAX (100%)",
+        "address": "5",
+        "tags": "GALAXY-X,SINGLE-ASSET"
+    },
+    {
+        "name": "USDC",
+        "composition": "USDC (100%)",
+        "address": "6",
+        "tags": "USDC,STABLE,SINGLE-ASSET"
+    },
+    {
+        "name": "MATIC",
+        "composition": "MATIC (100%)",
+        "address": "7",
+        "tags": "MATIC,SINGLE-ASSET"
+    },
+]
 
 module.exports = farmConfigs;
