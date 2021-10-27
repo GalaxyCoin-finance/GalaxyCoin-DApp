94ac7b626764dc167a658b20f3c09c2fdb4a9283
Timo <tklaasee@gmail.com>
2021-10-27 21:48:47 +0200

    Added QS lp addresses

--- src/utils/config.js
+++ src/utils/config.js
@@ -21,11 +21,13 @@ const configs = {
         gaxAddress: '0xC8A5BCb5D53CD824497b5381b592Bb747d6D27b3',
         farmAddress: '0x888ee59B74D95d9Bc936208F3ec0Bfb5825Fc20d',
         glxyPoolId: '0xb70c25d96ef260ea07f650037bf68f5d6583885e000100000000000000000048',
-        gaxLPAddress: '',
+        gaxLPAddress: '0x535f4987C013CC15E0055f652C077bE2006B3aBc',
 
         // other tokens info
         usdcAddress: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
-        balancerVaultAddress: '0xBA12222222228d8Ba445958a75a0704d566BF2C8'
+        balancerVaultAddress: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
+        quickswapRouterAddress: '0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff',
+        maticUsdc: '0x6e7a5fafcec6bb1e78bae2a1f0b612012bf14827'
     },
     local: {
         // put local contract addresses here
